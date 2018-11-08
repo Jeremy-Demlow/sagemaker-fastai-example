c04a8301ac9fc6268a43b7acd53de2c6b265990e
Andrew Shaw <andrew.t.shaw@gmail.com>
2018-11-07 18:00:34 -0800

    Initial style code

--- src/style/style.py
+++ src/style/style.py
@@ -25,6 +25,15 @@ import fastai
 from fastai import *
 from fastai.vision import *
 
+
+torch.backends.cudnn.benchmark=True
+import torch.distributed.deprecated as dist
+from torch.utils.data.distributed import DistributedSampler
+
+from models import *
+from loss import TransferLoss
+from data import ContentStyleLoader, InputDataset, SimpleDataBunch
+
 # setup the logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -33,68 +42,104 @@ logger.setLevel(logging.DEBUG)
 JSON_CONTENT_TYPE = 'application/json'
 JPEG_CONTENT_TYPE = 'image/jpeg'
 
-# define the classification classes
-CLASSES = ['metal', 'sport']
-
 # The train method
 def _train(args):
     print(f'Called _train method with model arch: {args.model_arch}, batch size: {args.batch_size}, image size: {args.image_size}, epochs: {args.epochs}')
     print(f'Getting training data from dir: {args.data_dir}')
-    data = ImageDataBunch.from_folder(args.data_dir, train=".", valid_pct=0.2,
-        ds_tfms=get_transforms(), size=args.image_size, num_workers=4, bs=args.batch_size).normalize(imagenet_stats)
-    print(f'Model architecture is {args.model_arch}')
-    arch = getattr(models, args.model_arch)
-    print("Creating pretrained conv net")
-    learn = create_cnn(data, arch, metrics=accuracy)
-    print("Fit four cycles")
-    learn.fit_one_cycle(4)
-    print(f'Unfreeze and run {args.epochs} more cycles')
-    learn.unfreeze()
-    learn.fit_one_cycle(args.epochs, max_lr=slice(3e-5,3e-4))
-    path = Path(args.model_dir)
-    print(f'Saving model weights to dir: {args.model_dir}')
-    learn.save(path/args.model_arch)
+    
+    # Create models
+    data = _load_data(args)
+    print('Loaded data')
+
+    # Create models
+    mt = StyleTransformer()
+    ms = StylePredict.create_resnet() if args.resnet else StylePredict.create_inception()
+    m_com = CombinedModel(mt, ms).cuda()
+    m_vgg = VGGActivations().cuda()
+
+    print('Created models')
+
+    # Training
+    opt_func = partial(Adam, betas=(0.9,0.999), weight_decay=1e-3)
+    st_wgt = 2.5e9
+    ct_wgt = 5e2    
+    tva_wgt = 1e-6
+    st_block_wgts = [1,80,200,5] # 2,3,4,5
+    c_block = 1 # 1=3
+
+    style_phases = [(1,1e2,st_wgt*2),(1,st_wgt,st_wgt/2)]*2 + [(epochs,st_wgt,st_wgt)]
+    cont_phases = [(1,ct_wgt,ct_wgt/2),(1,ct_wgt,ct_wgt*2)]*2 + [(epochs,ct_wgt,ct_wgt)]
+
+    loss_func = TransferLoss(m_vgg, ct_wgt, st_wgt, st_block_wgts, tva_wgt, c_block)
+
+    learner = Learner(data, m_com, opt_func=opt_func, loss_func=loss_func)
+    w_sched = partial(WeightScheduler, loss_func=loss_func, cont_phases=cont_phases, style_phases=style_phases)
+    recorder = partial(DistributedRecorder, save_path=args.save, print_freq=args.print_freq)
+    learner.callback_fns = [recorder, w_sched]
+
+    print('Begin training')
+    learner.fit_one_cycle(args.epochs, 5e-5)
+
+    print('Saving model weights to dir: {args.model_dir}')
+    # learn.save(Path(args.model_dir))
+    trace_data = torch.ones((1,3,64,64))
+    traced_model = torch.jit.trace(m_com, (trace_data, trace_data))
+    torch.jit.save(traced_model, Path(args.model_dir)/'test.pth')
+
+
+def _load_data(args):
+    # Content Data
+    content_files = get_files(args.content_dir, recurse=True)
+    style_files = get_files(args.style_dir, recurse=True)
+
+    train_dl = get_data(content_files, style_files, size=args.image_size, cont_bs=args.batch_size)
+    return SimpleDataBunch(train_dl, MODEL_PATH)
 
 # Return the Convolutional Neural Network model
 def model_fn(model_dir):
     logger.debug('model_fn')
-    fastai.defaults.device = torch.device('cpu')
-    # get the model architecture from name of saved model weights
-    arch_name = os.path.splitext(os.path.split(glob.glob(f'{model_dir}/resnet*.pth')[0])[1])[0]
-    print(f'Model architecture is: {arch_name}')
-    arch = getattr(models, arch_name)
-    empty_data = ImageDataBunch.single_from_classes(Path('/tmp'), CLASSES, 
-        tfms=get_transforms(), size=224).normalize(imagenet_stats)
-    learn = create_cnn(empty_data, arch, pretrained=False)
-    learn.load(Path(model_dir)/arch_name)
-    return learn
+    print('Model_fn called')
+    model_path = Path(args.model_dir)/'test.pth'
+    if not model_path.exists(): 
+        download_url('https://s3-us-west-2.amazonaws.com/ashaw-fastai-imagenet/fastai_style_lowerlr_8_jit.pth', model_path)
+    jit_model = torch.jit.load()
+    return jit_model
 
 # Deserialize the Invoke request body into an object we can perform prediction on
 def input_fn(request_body, content_type=JPEG_CONTENT_TYPE):
     logger.info('Deserializing the input data.')
+    print('Input fn')
     if content_type == JPEG_CONTENT_TYPE:
-        img = open_image(BytesIO(request_body))
-        return img
-    raise Exception('Requested unsupported ContentType in content_type: {}'.format(content_type))
+        con_fn = _tfm_img(BytesIO(request_body['content']))
+        style_fn = _tfm_img(BytesIO(request_body['style']))
+    elif content_type == JSON_CONTENT_TYPE:
+        con_fn = download_image(con_url, 'content.jpg')
+        style_fn = download_image(style_url, 'style.jpg')
+    else: raise Exception('Requested unsupported ContentType in content_type: {}'.format(content_type))
+    x_style = _tfm_img(style_fn).unsqueeze(0)
+    x_cont = _tfm_img(con_fn).unsqueeze(0)
+    return x_cont, x_style
+
+def _tfm_img(img_fn):
+    img = open_image(img_fn)
+    return data_norm((apply_tfms(style_tds.tfms, img.resize(size)).px,None))[0]
 
 # Perform prediction on the deserialized object, with the loaded model
 def predict_fn(input_object, model):
     logger.info("Calling model")
-    predict_class,_,predict_values = model.predict(input_object)
-    print(f'Predicted class is {predict_class}')
-    print(f'Predicted values are {predict_values}')
-    preds = F.softmax(predict_values, dim=0)
-    conf_score, indx = torch.max(preds, dim=0)
-    print(f'Softmax confidence score is {conf_score.item()}')
-    response = {}
-    response['class'] = predict_class
-    response['confidence'] = conf_score.item()
-    return response
+    x_con, x_style = input_object
+    print('Predicting')
+    with torch.no_grad(): 
+        out = model(x_con, x_style)
+    print('Predicted')
+    return Image((out[idx].detach().cpu())).px
 
 # Serialize the prediction result into the desired response content type
-def output_fn(prediction, accept=JSON_CONTENT_TYPE):        
+def output_fn(prediction, accept=JPEG_CONTENT_TYPE):        
     logger.info('Serializing the generated output.')
-    if accept == JSON_CONTENT_TYPE:
+    # if accept == JSON_CONTENT_TYPE:
+    #     return json.dumps(prediction), accept
+    if accept == JPEG_CONTENT_TYPE:
         return json.dumps(prediction), accept
     raise Exception('Requested unsupported ContentType in Accept: {}'.format(accept))    
 
@@ -125,6 +170,8 @@ if __name__ == '__main__':
     parser.add_argument('--current-host', type=str, default=os.environ['SM_CURRENT_HOST'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
     parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
+    parser.add_argument('--content-dir', type=str, default=os.environ['SM_CHANNEL_CONTENT'])
+    parser.add_argument('--style-dir', type=str, default=os.environ['SM_CHANNEL_STYLE'])
     parser.add_argument('--num-gpus', type=int, default=os.environ['SM_NUM_GPUS'])
 
     _train(parser.parse_args())
